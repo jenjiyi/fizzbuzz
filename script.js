b4d9b6e539c163525b1577be2048efab1a929ff5
jenjiyi <jenny.yamada@gmail.com>
2015-06-28 21:05:02 -0700

    added input and integer validation

--- script.js
+++ script.js
@@ -1,17 +1,42 @@
-// javascript file
-	for ( var number = 1; number <= 100; number++){
-			console.log(number);
-			if (number % 3 == 0 && number % 5 == 0){
-				document.write("FizzBuzz");
+$( document ).ready(function() {
+
+
+	function fizzBuzz(num){
+	for ( var i = 1; i <= num; i++){
+			console.log(i);
+			if (i % 3 == 0 && i % 5 == 0){
+				 $('.results').append("FizzBuzz");
 			}
-			else if (number % 3 == 0) {
-				document.write("Fizz");
+			else if (i % 3 == 0) {
+				$('.results').append("Fizz");
 			}
-			else if (number % 5 == 0) {
-				document.write("Buzz");
+			else if (i % 5 == 0) {
+				$('.results').append("Buzz");
 			}
 			else {
-			document.write(number);
-		};
-		document.write("<br>");
-		};
+			$('.results').append(i);
+		}
+		$('.results').append("<br>");
+		}
+} 
+
+//On click 
+  $('#item-submit').on('click', function(e){
+  	e.preventDefault();
+    var getVal =+ $('#item-input').val();
+    
+    //Checking if the typed value is a number
+    if (getVal % 1 != 0 || getVal > 100 || isNaN(getVal)){
+    	 alert('Type only numbers between 1 and 100.');
+      $('#item-input').val("");
+     
+    }else{
+      $('.results').empty();
+    	fizzBuzz(getVal);
+    	$('#item-input').val("");
+    }
+  });
+
+});
+
+
